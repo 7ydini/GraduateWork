e85c26e7fb77703563cb58ab669b6f6a0271b0ef
Lisiy Dmitriy <dlisiy@ds.ru>
2021-12-20 11:32:13 +0300

    Imports refactoring

--- src/main/java/ru/ds/GraduateWork/service/Impl/DefaultEmailService.java
+++ src/main/java/ru/ds/GraduateWork/service/Impl/DefaultEmailService.java
@@ -1,13 +1,11 @@
 package ru.ds.GraduateWork.service.Impl;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
